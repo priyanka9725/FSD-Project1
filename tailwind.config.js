5f95be43521be4e9b79559049b800d7a5a56778e
priyanka9725 <priyanka97xx@gmail.com>
2024-04-14 22:12:55 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+  letterSpacing:{
+    moreWider: '1rem',
+  }
+  }
+,
+  plugins: [],
+};
